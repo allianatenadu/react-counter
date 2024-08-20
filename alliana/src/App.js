88234ebd7397f86e02396570f9ebf42409a716b4
Aliana Tenadu <alianatenadu@gmail.com>
2024-08-20 15:21:34 +0000

    done

---
+++ alliana/src/App.js
@@ -0,0 +1,63 @@
+// import "./App.css";
+
+// function App() {
+//   const handleclick = () => {
+//     console.log("cilcked");
+//   };
+
+//   return (
+//     <div ClassName="App">
+//       <button onClick={handleclick}> Click</button>
+//     </div>
+//   );
+// }
+// export default App;
+
+// import { useState } from "react";
+
+// function App() {
+//   const [count, setCount] = useState(0); //for only number, for alphabetics and names ("")
+
+//   const handleClick = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <div ClassName="App">
+//       <h1>{count} 0</h1>
+//       <button onClick={handleClick}> Click</button>
+//     </div>
+//   );
+// }
+
+// //formthe cration of forms
+
+// import "./App.css";
+
+//  function App() {
+
+// const [name, setName]= useState("")
+// const [email, setEmail]= useState("")
+
+//     const handleName = (event) =>{
+//         setName(event.target.value)
+//     }
+//     const handleEmail = (event) =>{
+
+//     }
+//     const HandleSumbit = (event) =>{
+
+//     }
+
+//     return(
+//         <div ClassName="App">
+
+//         <form onSumbit={handleSumbit}>
+//             <input onChange={handleName} value={name} placeholder="Enter name" type="text"/>
+//             <input onChange={handleEmail}/>
+//             <input onChange={handleSumbit}/>
+//         </form>
+
+//     </div>
+//     )
+//  }
